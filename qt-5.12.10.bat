43a8556e3a16800af468f64dfa4c59a119e69c8e
Julien Schueller <schueller@phimeca.com>
2022-02-02 16:40:01 +0100

    qt-5.12.10.bat

---
+++ qt-5.12.10.bat
@@ -0,0 +1,6 @@
+C:\work\qtbase-everywhere-src-5.12.10\configure.bat -platform win32-msvc -opensource -confirm-license -release -no-angle -opengl desktop -nomake examples -nomake tests -prefix "C:\work\SALOME-9.7.0\W64\qt" -verbose -openssl -openssl-linked OPENSSL_INCDIR="C:\work\SALOME-9.7.0\W64\openssl\include" OPENSSL_LIBDIR="C:\work\SALOME-9.7.0\W64\openssl\lib" OPENSSL_LIBS="-llibcrypto-1_1-x64 -llibssl-1_1-x64 -lWs2_32 -lGdi32 -lAdvapi32 -lCrypt32 -lUser32"
+
+nmake   
+nmake install
+
+exit 0
